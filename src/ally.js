0741c2fd94f500841eadbddbbe498ee0da2bb51e
Ferdinand Prantl <prantlf@gmail.com>
2018-08-23 15:37:17 +0200

    Make a custom build including "get", "is" and "query" APIs only
    DO not minify the output UMD file. Allow easier integration to projects, which bundle the script to a bigger file, which in turn will be minified and provided with the total source map.

--- src/ally.js
+++ src/ally.js
@@ -1,35 +1,35 @@
 
 // this builds up the UMD bundle
 
-import element from './element/_element';
-import event from './event/_event';
-import fix from './fix/_fix';
+// import element from './element/_element';
+// import event from './event/_event';
+// import fix from './fix/_fix';
 import get from './get/_get';
 import is from './is/_is';
-import maintain from './maintain/_maintain';
-import map from './map/_map';
-import observe from './observe/_observe';
+// import maintain from './maintain/_maintain';
+// import map from './map/_map';
+// import observe from './observe/_observe';
 import query from './query/_query';
-import style from './style/_style';
-import when from './when/_when';
-import version from './version';
+// import style from './style/_style';
+// import when from './when/_when';
+// import version from './version';
 
 // save current window.ally for noConflict()
 const conflicted = typeof window !== 'undefined' && window.ally;
 
 export default {
-  element,
-  event,
-  fix,
+  // element,
+  // event,
+  // fix,
   get,
   is,
-  maintain,
-  map,
-  observe,
+  // maintain,
+  // map,
+  // observe,
   query,
-  style,
-  when,
-  version,
+  // style,
+  // when,
+  // version,
   noConflict: function() {
     if (typeof window !== 'undefined' && window.ally === this) {
       window.ally = conflicted;
